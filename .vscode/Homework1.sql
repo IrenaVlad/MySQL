1f505a2ba0aa92bdfed1ca9033aed7fdb25bb726
IrenaVlad <iriscka@bk.ru>
2023-02-08 23:59:05 +0100

    changes

--- .vscode/Homework1.sql
+++ .vscode/Homework1.sql
@@ -26,8 +26,8 @@ SELECT id, product_name, manufacturer, price FROM mobils
 WHERE product_count > 2;
 
 --Выведите весь ассортимент товаров марки “Samsung”
-SELECT id, manufacturer, product_count, price FROM mobils
-WHERE product_name = 'Samsung';
+SELECT id, product_name, product_count, price FROM mobils
+WHERE manufacturer = 'Samsung';
 
 --С помощью регулярных выражений найти:
 	--4.1. Товары, в которых есть упоминание "Iphone"
@@ -39,4 +39,4 @@ WHERE manufacturer = 'Samsung';
 	--4.3.  Товары, в которых есть ЦИФРЫ
 	--4.4.  Товары, в которых есть ЦИФРА "8"  
 SELECT id, manufacturer, product_count, price FROM mobils
-WHERE product_name LIKE %8%;
+WHERE product_name LIKE '%8%';
